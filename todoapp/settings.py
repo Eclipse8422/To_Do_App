2527e86b9bbca7c4ff5cbbfd92a85a35403397ad
Harsh <harshkhantwaal786@gmail.com>
2025-01-24 21:27:18 +0530

    postgres

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
 ]
 
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
